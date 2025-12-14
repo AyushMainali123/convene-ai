ec386f6f39929a751601d018184d4e1854fa5948
AyushMainali123 <ayushmainali11@gmail.com>
2025-12-14 11:19:54 +0545

    feat: used sandboxe env for all instance

--- src/lib/polar.ts
+++ src/lib/polar.ts
@@ -7,5 +7,5 @@ if (!process.env.POLAR_ACCESS_TOKEN) {
 
 export const polarClient = new Polar({
     accessToken: process.env.POLAR_ACCESS_TOKEN,
-    server: process.env.NODE_ENV !== "production" ? "sandbox" : "production"
+    server: "sandbox" 
 });
